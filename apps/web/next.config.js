18b249733702202d802a0b98c3ea166a6edecc56
phucthinhvn122 <phucthinhvn122@gmail.com>
2026-06-27 11:27:43 +0700

    Fix web deployment build config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  outputFileTracingRoot: require('path').join(__dirname, '../..'),
   transpilePackages: ['@surfshark/shared'],
 };
 module.exports = nextConfig;
